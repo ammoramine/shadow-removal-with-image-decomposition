f451ae2f95a081d7b07def17e74a0b68caaa178c
Amine AMMOR <aamineammor@gmail.com>
2021-08-14 17:37:38 +0200

    main code of shadow_detector

--- Code/models/shadow_detector.py
+++ Code/models/shadow_detector.py
@@ -22,16 +22,24 @@ import os
 dirFile = os.path.dirname(__file__)
 
 if __name__ == '__main__':
+
+    #TODO : understand why the folder BDRARImported must be kept in order to load the model
+    # why is there a binding between te saved model, and the pytohn code
     rel_path_img = "../../Data/ISTD_Dataset/train/train_A/11-1.png"
     path = os.path.join(dirFile,rel_path_img)
 
-    rel_path_model = "../../Data/BDRAR.pth"
-    path_model = os.path.join(dirFile,rel_path_model)
+    # rel_path_model = "../../Data/BDRAR.pth"
+    # path_model = os.path.join(dirFile,rel_path_model)
     img = Image.open(path)
     preprocess_img = img_transform(img)
     preprocess_img = preprocess_img.unsqueeze(0)
 
-    model = torch.load(path_model)
+    from models.BDRARImported import modelBDRAR
+    model = modelBDRAR.BDRAR().to("cuda")
+    rel_path_model = "../../Data/3000.pth"
+    path_model = os.path.join(dirFile,rel_path_model)
+    assert os.path.exists(path_model)
+    model.load_state_dict(torch.load(path_model,map_location=torch.device("cuda")))
     model = model.to("cpu")
     model.eval()
     out = model(preprocess_img).to("cpu").detach().numpy()
