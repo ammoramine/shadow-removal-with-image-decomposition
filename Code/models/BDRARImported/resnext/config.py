ccdd6073990aa4939e9da1ba95d94cd250b73a8b
EC2 Default User <ec2-user@ip-172-31-39-101.eu-west-3.compute.internal>
2021-08-22 11:39:40 +0000

    corrected error on paths

--- Code/models/BDRARImported/resnext/config.py
+++ Code/models/BDRARImported/resnext/config.py
@@ -1,1 +1,4 @@
-resnext_101_32_path = '..\\..\\..\\..\\Data\\resnext_101_32x4d.pth'
+import os
+dirFile = os.path.dirname(__file__)
+rel_resnext_101_32_path = '../../../../Data/resnext_101_32x4d.pth'
+resnext_101_32_path = os.path.join(dirFile,rel_resnext_101_32_path)
